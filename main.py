4b7eb0ec2a75b934c0256b76fd106928853255bb
wvunjo <joshua.webb4213@gmail.com>
2026-02-13 11:56:52 -0500

    E:Added a new optional command-line argument, --verbose. Imported types from google.genai created messages variable to create a new list of users messages and updated the call so contents=messages. Program currently is still "one-shot" but the code is updated to store a list of messages in the conversation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -13,18 +14,27 @@ def main():
     client = genai.Client(api_key = api_key)
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type = str, help = "User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    
+   
 
-    response = client.models.generate_content(model = "gemini-2.5-flash", contents = args.user_prompt)
+    
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
     # Defensive check
     if response.usage_metadata is None:
         raise RuntimeError("Gemini API returned no usage metadata. This may indicate a failed or incomplete API response."
                            )
+    
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    
     print(response.text)
 
 if __name__ == "__main__":
