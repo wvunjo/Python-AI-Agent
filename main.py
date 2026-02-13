26b6b10ad1985b5eb65319c879a38a3f4e503f02
wvunjo <joshua.webb4213@gmail.com>
2026-02-13 10:39:20 -0500

    D: User Input added rather than the hardcoded prompt for testing earlier. Imported argparse and now when running: uv run main.py "prompt goes here" will output per the string arugment you entered.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,15 +10,19 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
         raise RuntimeError("GEMINI_API_KEY environment variable not set, found, or is empty.")
-    client = genai.Client(api_key=api_key)
-    
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    client = genai.Client(api_key = api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type = str, help = "User prompt")
+    args = parser.parse_args()
+
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = args.user_prompt)
     # Defensive check
     if response.usage_metadata is None:
         raise RuntimeError("Gemini API returned no usage metadata. This may indicate a failed or incomplete API response."
                            )
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
+
     print(f"Prompt tokens: {prompt_tokens}")
     print(f"Response tokens: {response_tokens}")
     print(response.text)
