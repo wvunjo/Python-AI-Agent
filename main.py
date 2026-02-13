305e2b3d5e897796ba9fd687e369c925eefb4d5a
wvunjo <joshua.webb4213@gmail.com>
2026-02-13 10:01:16 -0500

    B: API setup completd with Gemini in main.py and added .env to .gitignore

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from python-ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    load_dotenv()
+    print("Hello from python-ai-agent!")
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set, found, or is empty.")
+    client = genai.Client(api_key=api_key)
+    
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
