d1f1eb00b01c654204812d04170aeee7903f0c88
wvunjo <joshua.webb4213@gmail.com>
2026-02-16 12:24:38 -0500

    H: calculator/lorem.txt was modified because the first test case wrote to that path. calculator/pkg/morelorem.txt was created as a new file (and the pkg directory was created by os.makedirs call) because of the second test case. Confirming write_file.py is functioning as intended and utilized the test_write_file.py correctly. Moving forward most likely all testing with .txt files will be ignored in later commits but this one was included to confirm successful test cases.

---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        common_path = os.path.commonpath([working_dir_abs, target_file])
+        parent_directory = os.path.dirname(target_file)
+
+
+        if common_path != working_dir_abs:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if os.path.isdir(target_file):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        os.makedirs(parent_directory, exist_ok=True)
+        
+        with open(target_file, "w", encoding="utf-8") as f:
+            f.write(content)
+        
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"
---
+++ test_write_file.py
@@ -0,0 +1,10 @@
+from functions.write_file import write_file
+
+result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+print(result)
+
+result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+print(result)
+
+result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+print(result)
