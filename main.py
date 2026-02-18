d7bcdba47b74ff095dee628b61471dd651ee31dc
wvunjo <joshua.webb4213@gmail.com>
2026-02-18 11:20:43 -0500

    O: Modified main.py to add the Agent Loop wrapping the entirety of the model-calling login in a for loop so the agent can iterate on a task until it's done working and has a final response for the user. Loop limits iterations to 20.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import argparse
 from dotenv import load_dotenv
@@ -20,53 +21,56 @@ def main():
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
-    
-   
-
-    
-    response = client.models.generate_content(
-        model = "gemini-2.5-flash",
-        contents = messages,
-        config = types.GenerateContentConfig(
-            tools = [available_functions], system_instruction=system_prompt),
-        )
-    # Defensive check
-    if response.usage_metadata is None:
-        raise RuntimeError("Gemini API returned no usage metadata. This may indicate a failed or incomplete API response."
-                           )
-    
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
     if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
+        print(f"User prompt: {args.user_prompt}\n")
     
-    function_results = []
+    for _ in range(20):
 
+        response = client.models.generate_content(
+            model = "gemini-2.5-flash",
+            contents = messages,
+            config = types.GenerateContentConfig(
+                tools = [available_functions], system_instruction=system_prompt),
+            )
+        # Defensive check
+        if response.usage_metadata is None:
+            raise RuntimeError("Gemini API returned no usage metadata. This may indicate a failed or incomplete API response."
+                            )
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        if args.verbose:
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {response_tokens}")
 
-    if response.function_calls:
-        for call in response.function_calls:
-            function_call= call_function(call, verbose=args.verbose)
-            if not function_call.parts:
-                raise Exception("The result should have parts!")
-            if function_call.parts[0].function_response is None:
-                raise Exception("The result should have parts!")
-            if function_call.parts[0].function_response.response is None:
-                raise Exception("The result should have parts!")
-            function_results.append(function_call.parts[0])
-            if args.verbose:
-                print(f"-> {function_call.parts[0].function_response.response}")
+        if not response.candidates:
+            raise RuntimeError("Gemini API returned no candidates.")
+        for candidate in response.candidates:
+            if candidate.content:
+                messages.append(candidate.content)
+        if response.function_calls:
+            function_response = []
+            for call in response.function_calls:
+                function_result = call_function(call, verbose=args.verbose)
 
-
-
-
+                if not function_result.parts:
+                    raise Exception("The function result should have parts!")
                 
-                
-            
+                tool_response_part = function_result.parts[0]
+                function_response.append(tool_response_part)
 
+        if response.function_calls:
+            messages.append(types.Content(role="user", parts=function_response))
+            continue
+        print(f"Final response:\n{response.text}")
+        return 
+    print("Conversation ended.")
+    sys.exit(1)
 
-            
+    
+    
+    
+    
+        
     
 
 if __name__ == "__main__":
